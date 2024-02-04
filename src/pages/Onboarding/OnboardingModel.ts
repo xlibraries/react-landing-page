aa263057149ed4df47687e657b6d251574d5abdf
Xlib <aman9893089064@gmail.com>
2024-02-05 02:29:54 +0530

    Updates

--- src/pages/Onboarding/OnboardingModel.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-export default {
-    currentSlide: 0,
-    slides: [
-        'Feature 1: This is the first feature of the application.',
-        'Feature 2: This is the second feature of the application.',
-        'Feature 3: This is the third feature of the application.'
-    ]
-};
